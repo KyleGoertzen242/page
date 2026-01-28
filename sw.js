129a3860705d5f2711cb7f550f0cd9be28b9e6c0
KyleGoertzen242 <kylegoertzen48@gmail.com>
2026-01-27 23:42:04 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10530768
+    "zoneId": 10530785
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
